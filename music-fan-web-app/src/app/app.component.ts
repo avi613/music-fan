61ee1c916e6de4e041fa4c384fef24e51f48747d
avi613 <avi.elkharrat@gmail.com>
2016-11-28 21:04:49 +0100

    select an artist

--- music-fan-web-app/src/app/app.component.ts
+++ music-fan-web-app/src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'music-fan-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Music Fan';
+  artist: Artist = {
+    id: 1,
+    fullName: 'Georges Brassens'
+  };
+  artists = ARTISTS;
+  selectedArtist: Artist;
+
+  onSelect(artist: Artist): void {
+    this.selectedArtist = artist;
+  }
 }
+
+export class Artist {
+  id: number;
+  fullName: string;
+}
+
+const ARTISTS: Artist[] = [
+  { id: 1, fullName: 'Georges Brassens' },
+  { id: 2, fullName: 'Edith Piaf' },
+  { id: 3, fullName: 'Robert Fripp' },
+  { id: 4, fullName: 'Michael Jackson' },
+  { id: 5, fullName: 'Leroi Moore' },
+  { id: 6, fullName: 'David Gilmour' },
+  { id: 7, fullName: 'Herbie Hancock' },
+  { id: 8, fullName: 'Tim Reynolds' },
+  { id: 9, fullName: 'Tito Puente' },
+  { id: 10, fullName: 'Vinnie Colaiuta' }
+]
